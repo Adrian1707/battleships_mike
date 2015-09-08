cc55cecc3ac7dad15700c00ac5b94691404274e7
Michael Roger <bodgeuk12@gmail.com>
2015-09-08 18:07:09 +0100

    Mapping works and ship onboard error works

--- lib/board.rb
+++ lib/board.rb
@@ -8,6 +8,7 @@ class Board
   end
 
   def ship_place(ship)
+    onboard_fail(ship)
     i = map.index(ship.location)
     n = 1
     ocean[i] = ship
@@ -21,6 +22,16 @@ class Board
       end
       n+=1
     end
+  end
+
+  def onboard_fail(ship)
+    board_width = ocean.count**0.5
+    i = map.index(ship.location)
+    o = ship.orientation.downcase
+
+    fail "Your ship is too big" if ship.size > board_width
+    fail "Your ship is on the land" if !ocean.include?(ship.location)
+    fail "Part of your ship is on land" if (o == "h" && i+ship.size > board_width) || (o == "v" && i+((board_width)*ship.size) > ocean.count)
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,8 +4,11 @@ require 'board'
 describe Board do
 
   let(:ship){double(:ship, size: 2, location: "A1", orientation: "H")}
+  let(:ship_big){double(:ship_big, size: 3, location: "A1", orientation: "H")}
+  let(:ship_out){double(:ship_out, size: 1, location: "A3", orientation: "H")}
+  let(:ship_off){double(:ship_off, size: 2, location: "B1", orientation: "V")}
 
-  it 'is created with an array(ocean) of length 4' do
+  it 'is created with an array(ocean)' do
     is_expected.to respond_to :ocean
   end
 
@@ -20,7 +23,18 @@ describe Board do
   it 'we would expect to see the ship in the ocean' do
     subject.ship_place(ship)
     expect(subject.ocean).to eq [ship,ship,"B1","B2"]
+  end
 
+  it 'raises an error if ship is too big (size 3) for the board' do
+    expect{ subject.ship_place(ship_big) }.to raise_error "Your ship is too big"
+  end
+
+  it 'raises an error if you place the ship outside of the board' do
+    expect{ subject.ship_place(ship_out) }.to raise_error "Your ship is on the land"
+  end
+
+  it 'raises an error if your ship has any part outside the board' do
+    expect{ subject.ship_place(ship_off) }.to raise_error "Part of your ship is on land"
   end
 
 
