a6f622876005a74978128bab8009a133cc61f51a
philip <philip.clarke@redhood.co.uk>
2015-09-08 20:00:35 +0100

    about to add the sunk method

--- lib/board.rb
+++ lib/board.rb
@@ -4,11 +4,12 @@ class Board
 
   def initialize
     @ocean = ["A1","A2","B1","B2"]
-    @map =  ocean
+    @map =  ["A1","A2","B1","B2"]
   end
 
   def ship_place(ship)
     onboard_fail(ship)
+    overlap_fail(ship)
     i = map.index(ship.location)
     n = 1
     ocean[i] = ship
@@ -24,14 +25,46 @@ class Board
     end
   end
 
+  def shoot(shot)
+    i = map.index(shot)
+    if ocean[i] != map[i]
+      print "Hit!"
+    else
+      print "Miss!"
+    end
+
+  end
+
+  private
+
   def onboard_fail(ship)
     board_width = ocean.count**0.5
     i = map.index(ship.location)
     o = ship.orientation.downcase
 
     fail "Your ship is too big" if ship.size > board_width
-    fail "Your ship is on the land" if !ocean.include?(ship.location)
-    fail "Part of your ship is on land" if (o == "h" && i+ship.size > board_width) || (o == "v" && i+((board_width)*ship.size) > ocean.count)
+    fail "Your ship is on the land" if !map.include?(ship.location)
+    fail "Part of your ship is on land" if (o == "h" && (i%board_width)+ship.size > board_width) || (o == "v" && (i/board_width.to_i)+ship.size > board_width)
   end
+
+  def overlap_fail(ship)
+    board_width = (ocean.count**0.5).to_i
+    i = map.index(ship.location)
+    n = 1
+    fail "You cannot place your ship on top of another ship" if ocean[i] != map[i]
+    while n < ship.size
+      if ship.orientation.downcase == "v"
+        i+=board_width
+        fail "You cannot place your ship on top of another ship" if ocean[i] != map[i]
+      elsif ship.orientation.downcase == "h"
+        i+=1
+        fail "You cannot place your ship on top of another ship" if ocean[i] != map[i]
+      end
+      n+=1
+    end
+
+  end
+
+
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,9 +4,11 @@ require 'board'
 describe Board do
 
   let(:ship){double(:ship, size: 2, location: "A1", orientation: "H")}
+  let(:ship2){double(:ship, size: 2, location: "A2", orientation: "V")}
   let(:ship_big){double(:ship_big, size: 3, location: "A1", orientation: "H")}
   let(:ship_out){double(:ship_out, size: 1, location: "A3", orientation: "H")}
-  let(:ship_off){double(:ship_off, size: 2, location: "B1", orientation: "V")}
+  let(:ship_off){double(:ship_off, size: 2, location: "A2", orientation: "H")}
+
 
   it 'is created with an array(ocean)' do
     is_expected.to respond_to :ocean
@@ -36,6 +38,37 @@ describe Board do
   it 'raises an error if your ship has any part outside the board' do
     expect{ subject.ship_place(ship_off) }.to raise_error "Part of your ship is on land"
   end
+
+  it 'raises and error if your any part of your ship is on the same part of the ocean as another ship' do
+    subject.ship_place(ship)
+    expect{ subject.ship_place(ship2) }.to raise_error "You cannot place your ship on top of another ship"
+  end
+
+  it "we would like to be able to fire a shot" do
+    is_expected.to respond_to :shoot
+  end
+
+  it "we would like the shot to have a coordinate" do
+    expect(subject).to respond_to(:shoot).with(1).argument
+  end
+
+  it "we would expect to register a hit if the shot hits part of a boat" do
+  subject.ship_place(ship)
+  expect{subject.shoot("A1")}.to output("Hit!").to_stdout
+  end
+
+  it "we would expect to register a miss if the shot hits part of a boat" do
+  subject.ship_place(ship)
+  expect{subject.shoot("B2")}.to output("Miss!").to_stdout
+  end
+
+  it "we would like to know when we have sunk an opponent's ship" do
+    subject.ship_place(ship)
+    subject.shoot("A2")
+    expect{subject.shoot("A1")}.to output("You have sunk my ship").to_stdout
+  end
+
+
 
 
 end
